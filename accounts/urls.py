26336214661f6c7dc5cdaf2c89f1c874cd082d87
Chinseok Lee <me@pyhub.kr>
2022-10-28 16:16:39 +0900

    로그인/로그아웃/프로필/회원가입 뷰 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from accounts import views
 
-urlpatterns = []
+urlpatterns = [
+    path("login/", views.login),
+    path("logout/", views.logout),
+    path("profile/", views.profile),
+    path("signup/", views.signup),
+]
